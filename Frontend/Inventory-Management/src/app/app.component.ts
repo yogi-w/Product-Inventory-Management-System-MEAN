b7b037c9554f325dad2b38a2e349a42868b388d7
yogi-w <yogisehz@gmail.com>
2026-01-03 20:13:48 +0530

    update file structure

--- Frontend/Inventory-Management/src/app/app.component.ts
+++ Frontend/Inventory-Management/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from './product.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,91 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Inventory-Management';
+
+
+  isUpdate: boolean = false;
+  editingProductCode: string | null = null;
+
+  productCode = '';
+  productName = '';
+  productCategory = '';
+  productPrice: any = '';
+
+  products: any[] = [];
+
+  constructor(private ps: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  // ADD / UPDATE 
+  submitProduct(form: any) {
+
+    const product = {
+      productCode: this.productCode,
+      productName: this.productName,
+      productCategory: this.productCategory,
+      productPrice: this.productPrice
+    };
+
+    if (this.isUpdate && this.editingProductCode) {
+
+      if (confirm('Are you sure to delete this product?')){
+          this.ps.updateProduct(this.editingProductCode, product)
+          .subscribe(() => {
+            this.isUpdate = false;
+            this.editingProductCode = null;
+            this.getProducts();
+            form.resetForm();
+          });
+
+      }
+
+     
+
+    } else {
+
+      this.ps.addProduct(product).subscribe(() => {
+        this.getProducts();
+        form.resetForm();
+      });
+
+    }
+  }
+
+  // EDIT
+  editProduct(p: any) {
+    this.isUpdate = true;
+    this.editingProductCode = p.productCode;
+
+    this.productCode = p.productCode;
+    this.productName = p.productName;
+    this.productCategory = p.productCategory;
+    this.productPrice = p.productPrice;
+  }
+
+  // CANCEL
+  clear(form: any) {
+    this.isUpdate = false;
+    this.editingProductCode = null;
+    form.resetForm();
+  }
+
+  // DELETE
+  deleteProduct(id: string) {
+    if (confirm('Are you sure to delete this product?')) {
+      this.ps.deleteProduct(id).subscribe(() => {
+        this.getProducts();
+      });
+    }
+  }
+
+  // GET 
+  getProducts() {
+    this.ps.getProduct().subscribe((res) => {
+      this.products = res;
+    });
+  }
 }
+
--- Frontend/Inventory-Management/src/app/app.module.ts
+++ Frontend/Inventory-Management/src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
 import {HttpClientModule} from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
